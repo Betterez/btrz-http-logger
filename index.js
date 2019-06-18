8459ff9c2e1606dd624eeb40b189340d8c316978
Marc Peters <mr.marc.peters@gmail.com>
2019-06-18 13:41:58 -0700

    #11ht47 Include process ID in logs

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 "use strict";
+const process = require("process");
 const morgan = require("morgan");
 
 module.exports = function _default(app, stream, name) {
-  morgan.token("instanceId", () => {
-    if (app.ec2Metadata) {
-      return app.ec2Metadata.instanceId || "missing";
-    }
-    return "missing";
+  morgan.token("serverId", () => {
+    const instanceId = (app.ec2Metadata && app.ec2Metadata.instanceId) || "missing";
+    const processId = `#${process.pid}`;
+
+    return `${instanceId}${processId}`;
   });
+
   morgan.token("traceId", (req) => {
     return (req.headers["x-amzn-trace-id"] || "").replace("=", "-");
   });
 
-  morgan.format("request-log", `[${name}-req] serverId=":instanceId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version referrer=":referrer" useragent=":user-agent"`);
-  morgan.format("combined-log", `[${name}-res] serverId=":instanceId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" responsetime=:response-time[1] date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version status=:status responselength=:res[content-length] referrer=":referrer" useragent=":user-agent"`);
+  morgan.format("request-log", `[${name}-req] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version referrer=":referrer" useragent=":user-agent"`);
+  morgan.format("combined-log", `[${name}-res] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" responsetime=:response-time[1] date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version status=:status responselength=:res[content-length] referrer=":referrer" useragent=":user-agent"`);
 
   app.use(morgan("request-log", {
     stream,
