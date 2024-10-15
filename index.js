8ac620c0e30b7b131b4c26d1fef4f4c40f90bf5e
Marc Peters <mr.marc.peters@gmail.com>
2024-10-15 15:18:26 -0700

    [MP] #86duwmmft Colorize request logs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 "use strict";
 const process = require("process");
 const chalk = require("chalk");
+const memoize = require("lodash.memoize");
 const morgan = require("morgan");
 
-const defaultColor = (...strings) => strings.join();
+const colorSchemes = {
+  NO_COLOR: "NO_COLOR",
+  DIM_TEXT: "DIM_TEXT",
+  RED_STATUS_CODE: "RED_STATUS_CODE",
+  MAGENTA_STATUS_CODE: "MAGENTA_STATUS_CODE"
+}
 
 module.exports = function _default(app, stream, name, config = {}) {
 
@@ -20,31 +26,69 @@ module.exports = function _default(app, stream, name, config = {}) {
     return (req.headers["x-amzn-trace-id"] || "").replace("=", "-");
   });
 
-  morgan.format("request-log", `[${name}-req] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version referrer=":referrer" useragent=":user-agent"`);
+  const getRequestLogFormatter = memoize((colorScheme) => {
+    let colorFn;
 
-  const combinedLogFormattersByColor = {};
+    switch (colorScheme) {
+      case colorSchemes.NO_COLOR:
+        colorFn = (logFormatString) => logFormatString;
+        break;
+      case colorSchemes.DIM_TEXT:
+        colorFn = (logFormatString) => chalk.dim(logFormatString);
+        break;
+      default:
+        throw new Error(`Unknown color scheme ${colorScheme}`);
+    }
+
+    return morgan.compile(
+      colorFn(`[${name}-req] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version referrer=":referrer" useragent=":user-agent"`)
+    );
+  });
+
+  morgan.format("request-log", function (tokens, req, res) {
+    let formatterFn = getRequestLogFormatter(colorSchemes.NO_COLOR);
+
+    if (config.colorize) {
+      formatterFn = getRequestLogFormatter(colorSchemes.DIM_TEXT);
+    }
+
+    return formatterFn(tokens, req, res);
+  });
+
+  const getCombinedLogFormatter = memoize((colorScheme) => {
+    let statusCodeColorFn;
+
+    switch (colorScheme) {
+      case colorSchemes.NO_COLOR:
+        statusCodeColorFn = (label, statusCode) => `${label}${statusCode}`;
+        break;
+      case colorSchemes.RED_STATUS_CODE:
+        statusCodeColorFn = (label, statusCode) => chalk.red(`${label}${chalk.bold(statusCode)}`);
+        break;
+      case colorSchemes.MAGENTA_STATUS_CODE:
+        statusCodeColorFn = (label, statusCode) => chalk.magenta(`${label}${chalk.bold(statusCode)}`);
+        break;
+      default:
+        throw new Error(`Unknown color scheme ${colorScheme}`);
+    }
+
+    return morgan.compile(
+      `[${name}-res] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" responsetime=:response-time[1] date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version ${statusCodeColorFn("status=", ":status")} responselength=:res[content-length] referrer=":referrer" useragent=":user-agent"`
+    );
+  });
+
   morgan.format("combined-log", function (tokens, req, res) {
-    let statusColor = "default";
-    let statusColorFn = defaultColor;
+    let formatterFn = getCombinedLogFormatter(colorSchemes.NO_COLOR);
 
     if (config.colorize) {
       if (res.statusCode >= 500) {
-        statusColor = "red";
-        statusColorFn = (label, statusCode) => chalk.red(`${label}${chalk.bold(statusCode)}`);
+        formatterFn = getCombinedLogFormatter(colorSchemes.RED_STATUS_CODE);
       } else if (res.statusCode >= 400) {
-        statusColor = "magenta";
-        statusColorFn = (label, statusCode) => chalk.magenta(`${label}${chalk.bold(statusCode)}`);
+        formatterFn = getCombinedLogFormatter(colorSchemes.MAGENTA_STATUS_CODE);
       }
     }
 
-    if (!combinedLogFormattersByColor[statusColor]) {
-      // Cache the formatter to avoid recompilation every time a log is emitted
-      combinedLogFormattersByColor[statusColor] = morgan.compile(
-        `[${name}-res] serverId=":serverId" remoteaddr=":remote-addr" xapikey=":req[x-api-key]" responsetime=:response-time[1] date=":date[iso]" traceId=":traceId" method=:method url=":url" http=:http-version ${statusColorFn("status=", ":status")} responselength=:res[content-length] referrer=":referrer" useragent=":user-agent"`
-      );
-    }
-
-    return combinedLogFormattersByColor[statusColor](tokens, req, res);
+    return formatterFn(tokens, req, res);
   });
 
   if (config.request) {
